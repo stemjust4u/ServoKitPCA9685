4b5a7eaf3ba23024abb5df81f77d455290fe179a
stemjust4u <stemjust4u@gmail.com>
2021-04-03 07:54:04 -0600

    update message function and mqtt_client

--- upython/boot.py
+++ upython/boot.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from umqttsimple import MQTTClient
 import ubinascii
 import machine
@@ -19,11 +19,10 @@ MQTT_SERVER = '10.0.0.115'
 MQTT_USER = stem[0] 
 MQTT_PASSWORD = stem[1] 
 MQTT_SUB_TOPIC1 = b'esp32Cam1/servo'
+MQTT_PUB_TOPIC1 = b'esp32Cam1/servo/status'
 MQTT_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 WIFI_SSID = stem[2]
 WIFI_PASSWORD = stem[3]
-topic_subSV = b'esp32Cam1/servo/vert'
-topic_subSH = b'esp32Cam1/servo/horiz'
 
 p5 = machine.Pin(5)
 p4 = machine.Pin(4)
@@ -33,11 +32,6 @@ servoH = machine.PWM(p5,freq=50)
 # 75 = 1.5mSec or neutral position
 servoV.duty(75) 
 servoH.duty(75)
-
-last_message = 0
-message_interval = 5
-daySleepTimer = 600000 # set deep sleep timer for 10min
-darkSleepTimer = 60000 # set deep sleep timer for 1min
 
 station = network.WLAN(network.STA_IF)
 
--- upython/main.py
+++ upython/main.py
@@ -1,8 +1,7 @@
 from machine import Pin, ADC
-from time import sleep
 import ujson
 
-def sub_cb(topic, msg):
+def on_message(topic, msg):
   #print("sub cd function %s %s %s" % (topic, msg, MQTT_SUB_TOPIC1))
   global newmsg, incomingD
   if topic == MQTT_SUB_TOPIC1:
@@ -18,7 +17,7 @@ def sub_cb(topic, msg):
 def connect_and_subscribe():
   global MQTT_CLIENT_ID, MQTT_SERVER, MQTT_SUB_TOPIC1, MQTT_USER, MQTT_PASSWORD
   client = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD)
-  client.set_callback(sub_cb)
+  client.set_callback(on_message)
   client.connect()
   client.subscribe(MQTT_SUB_TOPIC1)
   print('Connected to %s MQTT broker, subscribed to %s topic' % (MQTT_SERVER, MQTT_SUB_TOPIC1))
@@ -30,41 +29,41 @@ def restart_and_reconnect():
   machine.reset()
 
 try:
-  client = connect_and_subscribe()          # Connect and create the client
+  mqtt_client = connect_and_subscribe()          # Connect and create the client
 except OSError as e:
   restart_and_reconnect()
 
 # MQTT setup is successful.
 # Publish generic status confirmation easily seen on MQTT Explorer
 # Initialize dictionaries and start the main loop.
-client.publish(b"status", b"esp32 connected, entering main loop")
+mqtt_client.publish(b"status", b"esp32 connected, entering main loop")
 pin = 2
 led = Pin(pin, Pin.OUT) #2 is the internal LED
+led.value(1)
+sleep(1)
+led.value(0)  # flash led to know main loop starting
 outgoingD = {}
 incomingD = {}
 newmsg = True
 while True:
     try:
-      client.check_msg()
+      mqtt_client.check_msg()
       if newmsg:                              # INCOMING: New msg/instructions received
         for key, value in incomingD.items():
             direction = key
-            angle = value
+            duty = int(value)
             if direction == 'horiz':
-                kit.servo[0].angle = angle            # Drive servo on channel 0
-                sleep(0.1)           
+                servoH.duty(duty)
             elif direction == 'vert':
-                kit.servo[1].angle = angle     # Drive servo on channel 1
-                sleep(0.1)
-            outgoingD[direction + 'i'] = angle                
-            mqtt_client.publish(MQTT_PUB_TOPIC1, json.dumps(outgoingD))
-        newmsg = False          
+                servoV.duty(duty)
+            outgoingD[direction + 'i'] = duty
+            mqtt_client.publish(MQTT_PUB_TOPIC1, ujson.dumps(outgoingD))
+        newmsg = False
         #Uncomment prints for debugging. Will unpack the dictionary and then the converted JSON payload
         #print("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
         #for key, value in outgoingD.items():
         #    print("{0}:{1}".format(key, value))
         #print("Converted msg published on topic(tag): {0}".format(MQTT_PUB_TOPIC1))
         #print("JSON payload: {0}\n".format(ujson.dumps(outgoingD)))
-      sleep(0.1)
     except OSError as e:
       restart_and_reconnect()
