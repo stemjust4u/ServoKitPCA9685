b948d4350036902af80e53249b3964b8e7b11245
sj4u <stemjust4u@gmail.com>
2021-04-27 22:31:05 -0600

    updated on_message with regex

--- upyServo/boot.py
+++ upyServo/boot.py
@@ -1,45 +1,9 @@
-from time import sleep
-from umqttsimple import MQTTClient
-import ubinascii
-import machine
-import micropython
-import network
-import esp
+import esp, machine
 esp.osdebug(None)
-import gc
-gc.collect()
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
     print('woke from a deep sleep')
 
-with open("stem", "rb") as f:
-  stem = f.read().splitlines()
-
-MQTT_SERVER = '10.0.0.115'
-MQTT_USER = stem[0] 
-MQTT_PASSWORD = stem[1] 
-MQTT_SUB_TOPIC1 = b'esp32Cam1/servo'
-MQTT_PUB_TOPIC1 = b'esp32Cam1/servo/status'
-MQTT_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-WIFI_SSID = stem[2]
-WIFI_PASSWORD = stem[3]
-
-p5 = machine.Pin(19)
-p4 = machine.Pin(21)
-servoV = machine.PWM(p4,freq=50)
-servoH = machine.PWM(p5,freq=50)
-# initialize to neutral position
-# 75 = 1.5mSec or neutral position
-servoV.duty(75) 
-servoH.duty(75)
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(WIFI_SSID, WIFI_PASSWORD)
-
-while station.isconnected() == False:
-  pass
-
-print('Connection successful')
-print(station.ifconfig())
+machine.freq(240000000) # Can use 160000000 or 80000000 to drop power consumption by 10-20mA (almost 50%)
+cpufreqi = machine.freq()/(10**9)
+print('{0}MHz'.format(cpufreqi))
--- upyServo/main.py
+++ upyServo/main.py
@@ -1,69 +1,166 @@
-from machine import Pin, ADC
-import ujson
+import utime, ubinascii, micropython, network, re, ujson
+from lib.umqttsimple import MQTTClient
+from machine import Pin, PWM
+import gc
+gc.collect()
+micropython.alloc_emergency_exception_buf(100)
 
-def on_message(topic, msg):
-  #print("sub cd function %s %s %s" % (topic, msg, MQTT_SUB_TOPIC1))
-  global newmsg, incomingD
-  if topic == MQTT_SUB_TOPIC1:
-    incomingD = ujson.loads(msg.decode("utf-8", "ignore")) # decode json data to dictionary
-    newmsg = True
-    #Uncomment prints for debugging. Will print the JSON incoming payload and unpack the converted dictionary
-    #print("Received topic(tag): {0}".format(topic))
-    #print("JSON payload: {0}".format(msg.decode("utf-8", "ignore")))
-    #print("Unpacked dictionary (converted JSON>dictionary)")
-    #for key, value in incomingD.items():
-    #  print("{0}:{1}".format(key, value))
+def connect_wifi(WIFI_SSID, WIFI_PASSWORD):
+    station = network.WLAN(network.STA_IF)
 
-def connect_and_subscribe():
-  global MQTT_CLIENT_ID, MQTT_SERVER, MQTT_SUB_TOPIC1, MQTT_USER, MQTT_PASSWORD
-  client = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD)
-  client.set_callback(on_message)
-  client.connect()
-  client.subscribe(MQTT_SUB_TOPIC1)
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (MQTT_SERVER, MQTT_SUB_TOPIC1))
-  return client
+    station.active(True)
+    station.connect(WIFI_SSID, WIFI_PASSWORD)
 
-def restart_and_reconnect():
-  print('Failed to connect to MQTT broker. Reconnecting...')
-  sleep(10)
-  machine.reset()
+    while station.isconnected() == False:
+        pass
 
-try:
-  mqtt_client = connect_and_subscribe()          # Connect and create the client
-except OSError as e:
-  restart_and_reconnect()
+    print('Connection successful')
+    print(station.ifconfig())
 
-# MQTT setup is successful.
-# Publish generic status confirmation easily seen on MQTT Explorer
-# Initialize dictionaries and start the main loop.
-mqtt_client.publish(b"status", b"esp32 connected, entering main loop")
-pin = 2
-led = Pin(pin, Pin.OUT) #2 is the internal LED
-led.value(1)
-sleep(1)
-led.value(0)  # flash led to know main loop starting
-outgoingD = {}
-incomingD = {}
-newmsg = True
-while True:
+def mqtt_setup(IPaddress):
+    global MQTT_CLIENT_ID, MQTT_SERVER, MQTT_USER, MQTT_PASSWORD, MQTT_SUB_TOPIC, MQTT_REGEX
+    with open("stem", "rb") as f:    # Remove and over-ride MQTT/WIFI login info below
+      stem = f.read().splitlines()
+    MQTT_SERVER = IPaddress   # Over ride with MQTT/WIFI info
+    MQTT_USER = stem[0]         
+    MQTT_PASSWORD = stem[1]
+    WIFI_SSID = stem[2]
+    WIFI_PASSWORD = stem[3]
+    MQTT_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+    MQTT_SUB_TOPIC = []
+    # Specific MQTT_SUB_TOPICS for ADC, servo, stepper are .appended below
+    MQTT_REGEX = rb'nred2esp/([^/]+)/([^/]+)' # b'txt' is binary format. Required for umqttsimple to save memory
+                                              # r'txt' is raw format for easier reg ex matching
+                                              # 'nred2esp/+' would also work but would not return groups
+                                              # () group capture. Useful for getting topic lvls in on_message
+                                              # [^/] match a char except /. Needed to get topic lvl2, lvl3 groups
+                                              # + will match one or more. Requiring at least 1 match forces a lvl1/lvl2/lvl3 topic structure
+                                              # * could also be used for last group and then a lvl1/lvl2 topic would also be matched
+
+def mqtt_connect_subscribe():
+    global MQTT_CLIENT_ID, MQTT_SERVER, MQTT_SUB_TOPIC, MQTT_USER, MQTT_PASSWORD
+    client = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD)
+    client.set_callback(mqtt_on_message)
+    client.connect()
+    print('(CONNACK) Connected to {0} MQTT broker'.format(MQTT_SERVER))
+    for topics in MQTT_SUB_TOPIC:
+        client.subscribe(topics)
+        print('Subscribed to {0}'.format(topics)) 
+    return client
+
+def mqtt_on_message(topic, msg):
+    global MQTT_REGEX, debugmqtt
+    global mqtt_servo_duty, mqtt_servoID
+    if debugmqtt: print("Received topic(tag): {0}".format(topic))
+    msgmatch = re.match(MQTT_REGEX, topic)
+    if msgmatch:
+        incomingD = ujson.loads(msg.decode("utf-8", "ignore")) # decode json data
+        incomingID = [msgmatch.group(0), msgmatch.group(1), msgmatch.group(2), type(incomingD)]
+        if incomingID[1] == b'servoZCMD':
+            mqtt_servoID = int(incomingID[2])
+            mqtt_servo_duty = int(incomingD)
+        # Will print the JSON incoming payload and unpack it for debugging
+        if debugmqtt:
+            incomingPayload = ujson.loads(msg.decode("utf-8", "ignore"))
+            print("Incoming JSON payload: {0}".format(incomingPayload))
+            if isinstance(incomingPayload, (str, bool, int, float)):
+                print('Incoming item:{0}'.format(incomingPayload))
+            elif isinstance(incomingPayload, list):
+                print('Incoming list')
+                for item in incomingPayload:
+                    print(item)
+            elif isinstance(incomingPayload, dict):
+                print('Incoming dict')
+                for key, value in incomingPayload.items():  
+                    print("{0}:{1}".format(key, value))
+
+def mqtt_reset():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    utime.sleep_ms(5000)
+    machine.reset()
+
+def create_servo(pinlist):
+    global MQTT_SUB_TOPIC, device, outgoingD, mqtt_servoID, mqtt_servo_duty
+    MQTT_SUB_TOPIC.append(b'nred2esp/servoZCMD/+')
+    device.append(b'servo')
+    outgoingD[b'servo'] = {}
+    outgoingD[b'servo']['send'] = False    # Servo does not send any data to nodered
+    mqtt_servoID = 0
+    mqtt_servo_duty = 0
+    servoArr = []
+    setupinfo = True
+    freq=50       # higher freq has lower duty resolution. esp32 can go from 1-40000 (40MHz crystal oscillator) 
+    neutral = 75  # initialize to neutral position, 75=1.5mSec at 50Hz. (75/50=1.5ms or 1.5ms/20ms period = 7.5% duty cycle)
+    for i, pin in enumerate(pinlist):
+        servoArr.append(PWM(Pin(pin),freq=50))
+        servoArr[i].duty(neutral)
+        pinsummary.append(pin)
+    if setupinfo: print('Servo:{0}'.format(servoArr))
+    return servoArr 
+
+def main():
+    global pinsummary
+    global debugmqtt                                  # Used for debugging mqtt messages
+    global mqtt_servoID, mqtt_servo_duty     # Servo variables used in mqtt on_message
+    global device, outgoingD                          # Containers setup in 'create' functions and used for Publishing mqtt
+    
+    #===== SETUP MQTT/DEBUG VARIABLES ============#
+    # Setup mqtt variables (topics and data containers) used in on_message, main loop, and publishing
+    # Further setup of variables is completed in specific 'create_device' functions
+    mqtt_setup('10.0.0.115')
+    device = []    # mqtt lvl2 topic category and '.appended' in create functions
+    outgoingD = {} # container used for publishing mqtt data
+    
+    # umqttsimple requires topics to be byte format. For string.join to work on topics, all items must be the same, bytes.
+    ESPID = b'/esp32A'  # Specific MQTT_PUB_TOPICS created at time of publishing using string.join (specifically lvl2.join)
+    MQTT_PUB_TOPIC = [b'esp2nred/', ESPID]
+  
+    # Frequency (ms) to check for msg
+    on_msg_timer_ms = 100
+    
+    debugmqtt = False        # Turn ON to print mqtt msg traffic.
+    
+    #=== SETUP DEVICES ===#
+    # Boot fails if pin 12 is pulled high
+    # Pins 34-39 are input only and do not have internal pull-up resistors. Good for ADC
+    # Items that are sent as part of mqtt topic will be binary (b'item)
+    pinsummary = []
+    
+    servopins = [22, 23]
+    servo = create_servo(servopins)
+
+    print('Pins in use:{0}'.format(sorted(pinsummary)))
+    #==========#
+    # Connect and create the client
     try:
-      mqtt_client.check_msg()
-      if newmsg:                              # INCOMING: New msg/instructions received
-        for key, value in incomingD.items():
-            direction = key
-            duty = int(value)
-            if direction == 'horiz':
-                servoH.duty(duty)
-            elif direction == 'vert':
-                servoV.duty(duty)
-            outgoingD[direction + 'i'] = duty
-            mqtt_client.publish(MQTT_PUB_TOPIC1, ujson.dumps(outgoingD))
-        newmsg = False
-        #Uncomment prints for debugging. Will unpack the dictionary and then the converted JSON payload
-        #print("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
-        #for key, value in outgoingD.items():
-        #    print("{0}:{1}".format(key, value))
-        #print("Converted msg published on topic(tag): {0}".format(MQTT_PUB_TOPIC1))
-        #print("JSON payload: {0}\n".format(ujson.dumps(outgoingD)))
+        mqtt_client = mqtt_connect_subscribe()
     except OSError as e:
-      restart_and_reconnect()
+        mqtt_reset()
+    # MQTT setup is successful, publish status msg and flash on-board led
+    mqtt_client.publish(b'status'.join(MQTT_PUB_TOPIC), b'esp32 connected, entering main loop')
+    # Initialize flags and timers
+    checkmsgs = False
+    sendmsgs = False    
+    t0onmsg_ms = utime.ticks_ms()
+    
+    while True:
+        try:
+            if utime.ticks_diff(utime.ticks_ms(), t0onmsg_ms) > on_msg_timer_ms:
+                checkmsgs = True
+                t0onmsg_ms = utime.ticks_ms()
+            
+            servo[mqtt_servoID].duty(mqtt_servo_duty) # Servo commands sent one-at-a-time
+            
+            if checkmsgs:
+                mqtt_client.check_msg()
+                checkmsgs = False
+                
+            if sendmsgs: # Place holder. Servo does not send any messages to nodered
+                sendmsgs = False
+                
+        except OSError as e:
+            mqtt_reset()
+
+if __name__ == "__main__":
+    # Run main loop            
+    main()
